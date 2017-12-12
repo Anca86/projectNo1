5dd8f936ca0adecc52eb1622eaa53b4643ed4c90
Ancar <ancar>
2017-12-12 10:22:45 +0200

    code for session cart
    code for session cart included

--- index.php
+++ index.php
@@ -1,27 +1,38 @@
 <?php
 require_once('common.php');
 
-$sql = "SELECT * from products RIGHT JOIN images on Id=productId ORDER BY Id";
+$sql = "SELECT * from productsnew";
 $result = $conn->query($sql);
-if($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $row["url"] = 
-        "http://". $_SERVER['HTTP_HOST'].'/project1/images/'.$row['imageId'].".jpg";
-        echo 
-            '<div class="product">
-                <div class="image">
-                    <img src="' . $row["url"]. '">
-                </div>' .
-                '<div class="productdetails">
-                    <div class="productTitle">' . $row["Title"] . '</div>
-                    <div class="productDescription">' . $row["Description"] . '</div>
-                    <div class="productPrice">' . $row["Price"] . '</div>
-                    <a href="index.php?action=add" method="GET" class="addTocart" name="'.$row["Id"].'">Add</a>
-                </div>
-            </div>';                
-    }
-} else {
-    return false;
+
+if(isset($_POST["add_to_cart"])) {
+    if(isset($_SESSION["cart"])) {
+        $item_array_id = array_column($_SESSION["cart"], "id");
+
+        if(!in_array($_GET["id"], $item_array_id)) {
+
+            $count = count($_SESSION["cart"]);
+            $item_array = array(
+                    "id" => $_GET["id"], 
+                    "url" => $_POST["hidden_url"],
+                    "title" => $_POST["hidden_title"],
+                    "description" => $_POST["hidden_description"],
+                    "price" => $_POST["hidden_price"]
+                );
+            $_SESSION["cart"][$count] = $item_array;
+        } else {
+            echo "I am already added";
+        }
+
+    } else {
+        $item_array = array(
+                "id" => $_GET["id"],
+                "url" => $_POST["hidden_url"],
+                "title" => $_POST["hidden_title"],
+                "description" => $_POST["hidden_description"],
+                "price" => $_POST["hidden_price"]
+            );
+        $_SESSION["cart"][0] = $item_array;
+    } 
 }
 
 $conn->close();
@@ -35,8 +46,31 @@ $conn->close();
 </head>
 <body>
 
+
+<?php if($result->num_rows > 0): ?>
+    <?php while($row = $result->fetch_assoc()):?>
+            <div class="product">
+            <form method="post" action="index.php?action=add&amp;id=<?=$row['Id'] ?>">
+                <div class="image">
+                    <img src="<?= $row["Image"];?>">
+                </div>
+                <div class="productdetails">
+                    <div class="productTitle"><?= $row["Title"]?></div>
+                    <div class="productDescription"><?=$row["Description"]?></div>
+                    <div class="productPrice"><?=$row["Price"]?></div>
+                    <input type="hidden" name="hidden_url" value="<?= $row["url"]?>">
+                    <input type="hidden" name="hidden_title" value="<?= $row["Title"]?>">
+                    <input type="hidden" name="hidden_description" value="<?=$row["Description"]?>">
+                    <input type="hidden" name="hidden_price" value="<?=$row["Price"]?>">
+                    <input type="submit" name="add_to_cart" value="Add">
+                </div>
+            </form>
+            </div>               
+    <?php endwhile; ?>
+<?php endif;?>
+
 <div class="linkToCart">
-    <a href="cart.php">Go to cart</a>
+    <a href="cart.php"><?=translate("Go to cart")?></a>
 </div>
 
 </body>
